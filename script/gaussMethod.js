100ab1fdf865bae1e1315fb09bdc50c54d74f05d
AnnaMizgailo <102236391+AnnaMizgailo@users.noreply.github.com>
2025-02-05 01:22:46 +0300

    feat: added solution steps after results shown

--- script/gaussMethod.js
+++ script/gaussMethod.js
@@ -23,10 +23,18 @@ function generateFieldOfVariables(){
     div.innerHTML = htmlCode;
 };
 
-function checkIfAllInputsAreInserted(){
-    const numOfCols = document.getElementById("gauss_cols").value;
-    const numOfRows = document.getElementById("gauss_rows").value;
+function printMatrix(matrix, numOfRows, numOfCols){
+    let str = '<div>';
+    for(i = 0; i < numOfRows; i++){
+        for(j = 0; j < numOfCols; j++){
+                str += Math.round(matrix[i][j]* 1000)/1000 + " ";
+        }
+        str += "| " + Math.round(matrix[i][j]*1000)/1000 + '</div>';
+    }
+    return str;
+}
 
+function checkIfAllInputsAreInserted(numOfRows, numOfCols){
     for(i = 1; i <= numOfRows; i++){
         for(j = 1; j <= numOfCols; j++){
             if(document.getElementById("x"+i+j).value == ""){
@@ -39,14 +47,10 @@ function checkIfAllInputsAreInserted(){
             return;
         }
     }
-
-    alert("Вы ввели все коэффициенты!");
 };
 
-function formMatrix(){
+function formMatrix(numOfRows, numOfCols){
     let matrix = [];
-    const numOfCols = document.getElementById("gauss_cols").value;
-    const numOfRows = document.getElementById("gauss_rows").value;
 
     for(i = 1; i <= numOfRows; i++){
         let arr = [];
@@ -60,22 +64,26 @@ function formMatrix(){
     return matrix;
 }
 
-function makeReducedEchelonRowFormOfMatrix(matrix){
+function makeReducedEchelonRowFormOfMatrix(matrix, numOfRows, numOfCols, div){
         let lead = 0; 
-        const numOfCols = +document.getElementById("gauss_cols").value + 1;
-        const numOfRows = +document.getElementById("gauss_rows").value;
         const tolerance = 1e-10;
+        let htmlCode = '';
+        htmlCode += "<div class='solution'> <div>Дана матрица:</div>";
+        htmlCode += printMatrix(matrix, numOfRows, numOfCols - 1) + '</div>';
     
         for (let r = 0; r < numOfRows; r++) {
-            if (lead >= numOfCols) return matrix;
-    
+            if (lead >= numOfCols){
+                return matrix;
+            }
             let i = r;
             while (Math.abs(matrix[i][lead]) < tolerance) {
                 i++;
                 if (i === numOfRows) {
                     i = r;
                     lead++;
-                    if (lead === numOfCols) return matrix;
+                    if (lead === numOfCols){
+                        return matrix;
+                    } 
                 }
             }
     
@@ -86,6 +94,8 @@ function makeReducedEchelonRowFormOfMatrix(matrix){
                 for (let j = 0; j < numOfCols; j++) {
                     matrix[r][j] /= pivot;
                 }
+                htmlCode += "<div class='solution'> <div>Разделим " + +(r + 1) + ' строку матрицы на ' + Math.round(pivot*1000)/1000 + ':</div>';
+                htmlCode +=  printMatrix(matrix, numOfRows, numOfCols - 1) + '</div>';
             }
     
             for (let i = 0; i < numOfRows; i++) {
@@ -93,32 +103,43 @@ function makeReducedEchelonRowFormOfMatrix(matrix){
                     let factor = matrix[i][lead];
                     for (let j = 0; j < numOfCols; j++) {
                         matrix[i][j] -= factor * matrix[r][j];
-                    }
+                    }  
+                    htmlCode += "<div class='solution'><div>Отнимем от элементов " + +(i + 1) + ' строки матрицы элементы ' + +(r + 1) + ' строки матрицы, умноженной на ' + Math.round(factor*1000)/1000 + ':</div>';
+                    htmlCode +=  printMatrix(matrix, numOfRows, numOfCols - 1) + '</div>';
                 }
             }
     
             lead++;
         }
+
+        div.innerHTML = htmlCode;
+        div.style.display = "flex";
     
         return matrix;
 }
 
-
+function deriveResultForSquareMatrix(matrix, numOfRows, numOfCols, div){
+    let htmlCode = "";
+    for(i = 1; i <= numOfRows; i++){
+        htmlCode +=  '<div>x' + i + " = " + Math.round(matrix[i - 1][numOfCols]*1000)/1000 + "</div>";
+    }
+    div.innerHTML = htmlCode; 
+    div.style.display = "block";   
+}
 
 function calculateEquation(){
     const numOfCols = document.getElementById("gauss_cols").value;
     const numOfRows = document.getElementById("gauss_rows").value;
-    const div = document.getElementById("result");
+    const divResult = document.getElementById("result");
+    const divSolution = document.getElementById("solution");
 
-    checkIfAllInputsAreInserted();
+    checkIfAllInputsAreInserted(numOfRows, numOfCols);
 
-    let matrix = makeReducedEchelonRowFormOfMatrix(formMatrix());
-    let htmlCode = "";
+    let matrix = makeReducedEchelonRowFormOfMatrix(formMatrix(numOfRows, numOfCols), numOfRows, +numOfCols + 1, divSolution);
 
     if(numOfCols == numOfRows){
-        for(i = 1; i <= numOfRows; i++){
-            htmlCode +=  '<div>x' + i + " = " + matrix[i - 1][numOfCols] + "</div>";
-        }
+       deriveResultForSquareMatrix(matrix, numOfRows, numOfCols, divResult);
+       return;
     }
-    div.innerHTML = htmlCode;    
+   
 }
