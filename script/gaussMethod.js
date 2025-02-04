d364023707e0109088cb1daa86fc258b7a571cb0
AnnaMizgailo <102236391+AnnaMizgailo@users.noreply.github.com>
2025-02-04 14:05:01 +0300

    Added checking on input fields

--- script/gaussMethod.js
+++ script/gaussMethod.js
@@ -1,5 +1,6 @@
-function generateFieldOfVariables(){
+generateFieldOfVariables();
 
+function generateFieldOfVariables(){
     const numOfCols = document.getElementById("gauss_cols").value;
     const numOfRows = document.getElementById("gauss_rows").value;
     const div = document.getElementById("equation_container");
@@ -9,7 +10,7 @@ function generateFieldOfVariables(){
         htmlCode += ` <div class="string">`;
 
         for(j = 1; j <= numOfCols; j++){
-            htmlCode+=`<span class="variable"> <input type="number" class="coefficient" id="x` + i + j + `">x` + j;
+            htmlCode+=`<span class="variable"> <input type="number" class="coefficient" id="x` + i + j + `"> x` + j;
 
             if(j != numOfCols){
                     htmlCode += ` +`;
@@ -17,11 +18,31 @@ function generateFieldOfVariables(){
 
             htmlCode += ` </span>`;
         }
-        htmlCode += `=    <input type="number" class="coefficient" id="result` + i + `"></div>`;
-    
+        htmlCode += `=<input type="number" class="coefficient" id="result` + i + `"></div>`;
     }
-    console.log(htmlCode);
     div.innerHTML = htmlCode;
-    
-
 };
+
+function checkIfAllInputsAreInserted(){
+    const numOfCols = document.getElementById("gauss_cols").value;
+    const numOfRows = document.getElementById("gauss_rows").value;
+
+    for(i = 1; i <= numOfRows; i++){
+        for(j = 1; j <= numOfCols; j++){
+            if(document.getElementById("x"+i+j).value == ""){
+                alert("Вы ввели не все значения коэффициентов");
+                return;
+            }
+        }
+        if(document.getElementById("result"+i).value == "") {
+            alert("Вы ввели не все значения уравнения");
+            return;
+        }
+    }
+
+    alert("Вы ввели все коэффициенты!");
+};
+
+function calculateEquation(){
+    checkIfAllInputsAreInserted();
+}
